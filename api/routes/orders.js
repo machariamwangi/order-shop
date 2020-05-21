0f6d18b9075d8f4ec905776f058811c465da0245
machariamwangi <elijah.mwangi@binary.co.ke>
2020-05-21 17:49:08 -0400

    Version 0.0.3 reqheaders and response body

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next)=>{
 });
 
 router.post('/', (req, res, next)=>{
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(201).json({
-        message: "Orders waas created"
+        message: "Orders waas created",
+        createdOrder: order
     })
 });
 
